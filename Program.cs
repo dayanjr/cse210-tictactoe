57b855f4ac162300dd6b82ee80907981f9e9b432
dayanjr <dayanfrazao@byui.edu>
2022-09-27 21:35:09 -0600

    up

--- Program.cs
+++ Program.cs
@@ -63,6 +63,12 @@ class TicTacToe
     /// <returns></returns>
     static bool IsWinner(List<string> board, string player)
     {
+        if (board[0] == player && board[1] == player && board[2] == player) {
+            return true;
+        }
+        if (board[3] == player && board[4] == player && board[5] == player) {
+            return true;
+        }
         return false;
     }
 
@@ -71,23 +77,43 @@ class TicTacToe
     /// <returns>True if the board is full.</returns>
     static bool IsTie(List<string> board)
     {
-        return false;
+        bool foundDigit = false;
+        foreach (string value in board)
+        {
+            if (char.IsDigit(value[0]))
+            {
+                foundDigit = true;
+                break;
+            }
+        }
+        
+        return !foundDigit;
     }
 
     /// <summary>Cycles through the players (from x to o and o to x)</summary>
     /// <param name="currentPlayer">The current players sign (x or o)</param>
     /// <returns>The next players sign (x or o)</returns>
     static string GetNextPlayer(string currentPlayer)
-    {
-        return "x";
-    }
+    { string nextPlayer = "x";
+
+            if (currentPlayer == "x")
+            {
+                nextPlayer = "o";
+            }
+
+            return nextPlayer;
+        }
+    
 
     /// <summary>Gets the 1-based spot number associated with the user's choice.</summary>
     /// <param name="currentPlayer">The sign (x or o) of the current player.</param>
     /// <returns>A 1-based spot number (not a 0-based index)</returns>
     static int GetMoveChoice(string currentPlayer)
-    {
-        return 1;
+    {Console.Write($"{currentPlayer}'s turn to choose a square (1-9): ");
+            string move_string = Console.ReadLine();
+
+            int choice = int.Parse(move_string);
+            return choice;
     }
 
     /// <summary>
@@ -98,7 +124,13 @@ class TicTacToe
     /// <param name="choice">The 1-based spot number (not a 0-based index).</param>
     /// <param name="currentPlayer">The current player's sign (x or o)</param>
     static void MakeMove(List<string> board, int choice, string currentPlayer)
-    {
+    {// Convert the 1-based spot number to a 0-based index.
+            int index = choice - 1;
+
+            // It would be good to include an additional check here to ensure that
+            // the spot is available.
+            board[index] = currentPlayer;
+    
 
     }
 }
